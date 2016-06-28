b10c7472fb94da54736ca53443de3d03f464099d
Gerard Braad <me@gbraad.nl>
2016-06-28 22:06:19 +0800

    Update Ubuntu to use Ansible

--- ubuntu/run_container.sh
+++ ubuntu/run_container.sh
@@ -2,12 +2,17 @@
 
 # Install
 apt-get update
-apt-get install -y python python-openstackclient
+apt-get install -y python curl ansible
+
+# Deploy openstackclient
+curl -sSL https://raw.githubusercontent.com/gbraad/ansible-playbooks/master/playbooks/install-openstack-client.yml -o /tmp/install-openstack-client.yml
+ansible-playbook /tmp/install-openstack-client.yml
 
 # Install tools
 wget https://raw.githubusercontent.com/gbraad/openstack-tools/master/stack -P /sbin/
 chmod u+x /sbin/stack
 
 # Cleanup
+apt-get remove -y --auto-remove curl ansible 
 apt-get clean
 rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /root/.cache
